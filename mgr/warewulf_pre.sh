32d51a2382c5b9c666161aca1c7d7ae4ea6c9076
Cooper, Trevor <tcooper@sdsc.edu>
2021-04-14 18:18:52 -0700

    fixup /etc/hosts

--- mgr/warewulf_pre.sh
+++ mgr/warewulf_pre.sh
@@ -9,3 +9,22 @@ sudo setenforce permissive
 # Disable firewall
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
+
+# Fixup /etc/hosts which is mangled by Vagrant
+# See: https://github.com/hashicorp/vagrant/issues/7263
+# Remove entries created by vagrant:
+sudo sed -i "/$(hostname)/d" /etc/hosts
+
+# Remove entry from base box
+sudo sed -i "/centos8/d" /etc/hosts
+
+# Create the entries we want
+HOSTS_TMPL=/tmp/hosts
+(
+cat << 'TEMPLATE'
+
+192.168.15.15    mgr.example.com mgr
+
+TEMPLATE
+) > $HOSTS_TMPL
+sudo cat /tmp/hosts >> /etc/hosts
