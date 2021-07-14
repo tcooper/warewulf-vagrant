959a4c1aded3153dd5f3df3930d83abdc041e54a
Trevor Cooper <tcooper@rockylinux.org>
2021-07-14 08:24:53 -0700

    add support to create VirtualBox natnetwork during bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,10 @@ set -x
 
 ISO="http://boot.ipxe.org/ipxe.iso"
 
+NATNET_NAME="warewulf-natnet"
+NATNET_PREFIX="192.168"
+NATNET_SUBNET="15"
+NATNET_NETWORK="${NATNET_PREFIX}.${NATNET_SUBNET}.0/24"
 
 # Download iPXE ISO to support warewulf bzImage
 
@@ -16,5 +20,17 @@ popd
 
 # Add vagrant plugins
 vagrant plugin install vagrant-reload
+
+
+# Add VirtualBox NAT Network
+
+VBoxManage list natnets | grep "${NATNET_NAME}" >/dev/null 2>&1
+ret=$?
+if [[ "$ret" -ne 0 ]]; then
+  VBoxManage natnetwork add --netname "${NATNET_NAME}" --network "${NATNET_NETWORK}" --enable --dhcp off
+  VBoxManage natnetwork start --netname "${NATNET_NAME}"
+else
+  printf "NAT network %s already exists.\n" "${NATNET_NAME}"
+fi
 
 exit 0
