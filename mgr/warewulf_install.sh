34f0ec00b46da877da39f2c1bd2c2ebca4a5edcc
Trevor Cooper <tcooper@rockylinux.org>
2021-12-17 11:07:52 -0800

    affix build to version 4.2.0

--- mgr/warewulf_install.sh
+++ mgr/warewulf_install.sh
@@ -8,5 +8,6 @@ sudo dnf -y install golang tftp-server dhcp-server nfs-utils
 # Download build and install Warewulf
 git clone https://github.com/ctrliq/warewulf.git
 cd ./warewulf || exit
+git checkout v4.2.0
 make all
 sudo make install
