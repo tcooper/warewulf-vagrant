a80ec6eefdc85a2f334aedce90e7e8f47702e9a1
Cooper, Trevor <tcooper@sdsc.edu>
2021-12-17 17:02:57 -0800

    various container build script changes

--- mgr/warewulf_container_singularity_build_from_def.sh
+++ mgr/warewulf_container_singularity_build_from_def.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 CHROOT_DIR=/var/warewulf/chroots
 CHROOT_NAME=centos8-singularity-def
 BUILD_DIR="${HOME}/chroots/${CHROOT_NAME}"
+
+# This container build requires running distro to be CentOS
+if ! (awk -F\= '/^NAME/ {print $2}' /etc/os-release | grep -qi centos)
+then
+    printf "%s must be run on CentOS\n" "$0"
+    exit 0
+fi
 
 # Add singularity
 sudo dnf -y install singularity
@@ -31,8 +39,18 @@ cd "${BUILD_DIR}"
 sudo tar -cf - ./rootfs | (cd "${CHROOT_DIR}/${CHROOT_NAME}"; sudo tar -xf -)
 
 # Build new image
-sudo wwctl container build "${CHROOT_NAME}"
+sudo wwctl container build --force "${CHROOT_NAME}"
 sudo wwctl container list
+
+# Remove profile if it exists
+set +e
+sudo wwctl profile list | grep -q -E "^${CHROOT_NAME} "
+ret=$?
+set -e
+if [[ $ret -eq 0 ]]; then
+  # NOTE: wwctl profile delete could get a --yes option but this also works...
+  yes | sudo wwctl profile delete "${CHROOT_NAME}"
+fi
 
 # Create custom profile for container
 sudo wwctl profile add "${CHROOT_NAME}"
